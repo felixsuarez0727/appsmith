68ae599b617ec4dcf14e3d67ec2524464b34730b
Rimil Dey <rimil@appsmith.com>
2022-04-14 11:34:44 +0530

    Change the function name from postMessage -> postMessageToTargetWindow to prevent overriding worker thread's postMessage

--- app/client/src/workers/Actions.ts
+++ app/client/src/workers/Actions.ts
@@ -263,7 +263,11 @@ const DATA_TREE_FUNCTIONS: Record<
         };
       },
   },
-  postMessage: function(message: any, targetOrigin: string, transfer?: any[]) {
+  postMessageToTargetWindow: function(
+    message: any,
+    targetOrigin: string,
+    transfer?: any[],
+  ) {
     return {
       type: ActionTriggerType.POST_MESSAGE,
       payload: {
